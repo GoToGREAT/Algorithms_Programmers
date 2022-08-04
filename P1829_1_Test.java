8b1835bc4a6bcf1969d30722d773c33abc9aa6a1
codeeeeeeeeeeee2201 <codeeeeeeeeeeee2201@gmail.com>
2022-08-04 20:06:03 +0900

    programmers

--- P1829_1_Test.java
+++ P1829_1_Test.java
@@ -1,22 +1,24 @@
 import java.util.*;
 
-// 속도가 너무 느림
-public class P1829_2 {
-	class Solution {
-	    public int[] solution(int m, int n, int[][] picture) {
+
+public class P1829_1_Test {
+	public static void main(String[] args) {
+	    	int m = 6;
+	    	int n = 4;
+	    	int[][] picture = {{1, 1, 1, 0},{1, 2, 2, 0},{1, 0, 0, 1}, {0, 0, 0, 1}, {0, 0, 0, 3},{0, 0, 0, 3}};
 	    	boolean[][] boo = new boolean[m][n];
-	    	
 	        int numberOfArea = 0;
 	        int maxSizeOfOneArea = 0;
 	        
 	        while(true) {
 	        	Integer[] check = find_start(boo, m, n);
 	        	if(check==null) break;
+	        	System.out.println("조사시작 : "+check[0]+" / "+check[1]);
 	        	if(picture[check[0]][check[1]]==0) {
 	        		find_area(check, boo, picture, m, n);
 	        	} else {
 	        		numberOfArea++;
-	        		int now_max = find_area(check, boo, picture, numberOfArea, maxSizeOfOneArea);
+	        		int now_max = find_area(check, boo, picture, m,n);
 	        		maxSizeOfOneArea = maxSizeOfOneArea >= now_max ? maxSizeOfOneArea : now_max;
 	        	}
 	        }
@@ -24,14 +26,15 @@ public class P1829_2 {
 	        int[] answer = new int[2];
 	        answer[0] = numberOfArea;
 	        answer[1] = maxSizeOfOneArea;
-	        return answer;
+	        System.out.println("끝 : "+answer[0]+" / "+answer[1]);
 	    }
 	    
-	    Integer[] find_start(boolean[][] boo, int m, int n) {
+	  static Integer[] find_start(boolean[][] boo, int m, int n) {
 	    	for(int i=0; i<m; i++) {
 	    		for(int q=0; q<n; q++) {
 	    			if(boo[i][q]==false) {
 	    				Integer[] answer = {i,q};
+	    				boo[i][q] = true;
 	    				return answer;
 	    			}
 	    		}
@@ -39,14 +42,14 @@ public class P1829_2 {
 	    	return null;
 	    }
 	    
-	    int find_area(Integer[] start, boolean[][] boo, int[][] picture, int m, int n) {
+	   static int find_area(Integer[] start, boolean[][] boo, int[][] picture, int m, int n) {
 	    	int max = 1;
 	    	Stack<Integer[]> stack = new Stack<>();
 	    	stack.add(start);
 	    	
 	    	while(true) {
 	    		if(stack.isEmpty()) break;
-	    		Integer[] now =  stack.peek();
+	    		Integer[] now =  stack.pop();
 	    		int x = now[0];
 	    		int y = now[1];
 	    		
@@ -56,21 +59,21 @@ public class P1829_2 {
 	    			boo[x+1][y] = true;
 	    			Integer[] put = {x+1,y};
 	    			stack.add(put);
-	    		}
+	    		} 
 	    		if(y+1<n && boo[x][y+1]==false &&
 	    				picture[x][y+1] == picture[x][y]) {
 	    			max++;
 	    			boo[x][y+1] = true;
 	    			Integer[] put = {x,y+1};
 	    			stack.add(put);
-	    		}
+	    		} 
 	    		if(x-1>=0 && boo[x-1][y]==false &&
 	    				picture[x-1][y] == picture[x][y]) {
 	    			max++;
 	    			boo[x-1][y] = true;
 	    			Integer[] put = {x-1,y};
 	    			stack.add(put);
-	    		}
+	    		} 
 	    		if(y-1>=0 && boo[x][y-1]==false &&
 	    				picture[x][y-1] == picture[x][y]) {
 	    			max++;
@@ -81,5 +84,4 @@ public class P1829_2 {
 	    	}
 	    	return max;
 	    }
-	}
 }
--- P1829_2.java
+++ P1829_2.java
@@ -1,113 +1,86 @@
 import java.util.*;
 
-// m,n을 활용하였지만 오류 발생
-public class P1829_2 {
+// 참조자료형은 주소값 전달해주는거 신기하다.
+// 편리하다.
+public class P1829_1 {
 	class Solution {
-	    public int[] solution(int m, int n, int[][] picture) {	    	
-	    	FindAnswer expedition = new FindAnswer(picture, m, n);
+	    public int[] solution(int m, int n, int[][] picture) {
+	    	boolean[][] boo = new boolean[m][n];
 	    	
-	    	while(expedition.boo!=0) {
-	    		int[] now = expedition.serch_false();
-	    		expedition.serch_group(picture, now);
-	    	}
-	    	
-
-	        int[] answer = {expedition.color.size(), expedition.max_area};
+	        int numberOfArea = 0;
+	        int maxSizeOfOneArea = 0;
+	        
+	        while(true) {
+	        	Integer[] check = find_start(boo, m, n);
+	        	if(check==null) break;
+	        	if(picture[check[0]][check[1]]==0) {
+	        		find_area(check, boo, picture, m, n);
+	        	} else {
+	        		numberOfArea++;
+	        		int now_max = find_area(check, boo, picture, m, n);
+	        		maxSizeOfOneArea = maxSizeOfOneArea >= now_max ? maxSizeOfOneArea : now_max;
+	        	}
+	        }
+	         int[] answer = new int[2];
+	        answer[0] = numberOfArea;
+	        answer[1] = maxSizeOfOneArea;
 	        return answer;
 	    }
-    	class FindAnswer{
-    		int x_max;
-    		int y_max;
-    		
-    		int boo;		
-    		boolean[][] arr_boo;
-    		
-    		Set<Integer> color;
-    		int max_area;
-    		
-    		
-    		FindAnswer(int[][] picture, int m, int n){
-    			this.x_max = m;
-    			this.y_max = n;
-    			arr_boo = new boolean[x_max][y_max];
-    			boo = x_max*y_max;
-    			
-    			color = new HashSet<>();
-    			max_area = 0;
-    		}
-    		
-    		int[] serch_false() {
-    			for(int i=0; i<x_max; i++) {
-    				for(int k=0; k<y_max; k++) {
-    					if(arr_boo[i][k]==false) {
-    						int[] re = {i,k};
-    						return re;
-    					}
-    				}
-    			}
-    			return null;
-    		}
-    		
-    		void serch_group(int[][] picture, int[] start) {
-    			Stack<Integer[]> stack = new Stack<>();
-    			int start_x = start[0];
-    			int start_y = start[1];
-    			arr_boo[start_x][start_y]=true;
-    			boo--;
-    			Integer[] now = {start_x,start_y};
-    			stack.push(now);
-    			
-    			int now_color = picture[start_x][start_y];
-    			color.add(now_color);
-    			
-    			int count_area=1;
-    			
-    			while(true) {
-    				if(start_x+1<x_max && picture[start_x+1][start_y]==now_color
-    						&&arr_boo[start_x+1][start_y]==false) {
-    					arr_boo[start_x+1][start_y]=true;
-    					boo--;
-    					Integer[] a = {start_x+1,start_y};
-    					stack.push(a);
-    					start_x++;
-    					count_area++;
-    				} else if(start_y+1<y_max && picture[start_x][start_y+1]==now_color
-    						 &&arr_boo[start_x][start_y+1]==false) {
-    					arr_boo[start_x][start_y+1]=true;
-    					boo--;
-    					Integer[] a = {start_x,start_y+1};
-    					stack.push(a);
-    					start_y++;
-    					count_area++;
-    				} else if(start_x-1>=0 && picture[start_x-1][start_y]==now_color
-    						&&arr_boo[start_x-1][start_y]==false) {
-    					arr_boo[start_x-1][start_y]=true;
-    					boo--;
-    					Integer[] a = {start_x-1,start_y};
-    					stack.push(a);
-    					start_x--;
-    					count_area++;
-    				} else if(start_y-1>=0 && picture[start_x][start_y-1]==now_color
-    						 &&arr_boo[start_x][start_y-1]==false) {
-    					arr_boo[start_x][start_y-1]=true;
-    					boo--;
-    					Integer[] a = {start_x,start_y-1};
-    					stack.push(a);
-    					start_y--;
-    					count_area++;
-    				} else {
-    					if(stack.empty()) {
-    					if(now_color!=0) {
-    					max_area = max_area>=count_area ? max_area : count_area;	    					
-    					}
-    					break;
-    					}
-    					Integer[] new_start = stack.pop();
-    					start_x = new_start[0];
-    					start_y = new_start[1];
-    				}
-    			}
-    		}			
-    	}
+	    
+	    Integer[] find_start(boolean[][] boo, int m, int n) {
+	    	for(int i=0; i<m; i++) {
+	    		for(int q=0; q<n; q++) {
+	    			if(boo[i][q]==false) {
+	    				boo[i][q] = true;
+	    				Integer[] answer = {i,q};	    				
+	    				return answer;
+	    			}
+	    		}
+	    	}
+	    	return null;
+	    }
+	    
+	    int find_area(Integer[] start, boolean[][] boo, int[][] picture, int m, int n) {
+	    	int max = 1;
+	    	Stack<Integer[]> stack = new Stack<>();
+	    	stack.add(start);
+	    	
+	    	while(true) {
+	    		if(stack.isEmpty()) break;
+	    		Integer[] now = stack.pop();
+	    		int x = now[0];
+	    		int y = now[1];
+	    		
+	    		if(x+1<m && boo[x+1][y]==false &&
+	    				picture[x+1][y] == picture[x][y]) {
+	    			max++;
+	    			boo[x+1][y] = true;
+	    			Integer[] put = {x+1,y};
+	    			stack.add(put);
+	    		}
+	    		if(y+1<n && boo[x][y+1]==false &&
+	    				picture[x][y+1] == picture[x][y]) {
+	    			max++;
+	    			boo[x][y+1] = true;
+	    			Integer[] put = {x,y+1};
+	    			stack.add(put);
+	    		}
+	    		if(x-1>=0 && boo[x-1][y]==false &&
+	    				picture[x-1][y] == picture[x][y]) {
+	    			max++;
+	    			boo[x-1][y] = true;
+	    			Integer[] put = {x-1,y};
+	    			stack.add(put);
+	    		}
+	    		if(y-1>=0 && boo[x][y-1]==false &&
+	    				picture[x][y-1] == picture[x][y]) {
+	    			max++;
+	    			boo[x][y-1] = true;
+	    			Integer[] put = {x,y-1};
+	    			stack.add(put);
+	    		}
+	    	}
+	    	return max;
+	    }
 	}
 }
